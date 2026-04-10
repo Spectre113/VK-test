19f37e8c8005580166368c322cb6b446464d9770
Владимир Топорков <VTvolody626@mail.ru>
2026-04-10 09:20:34 +0300

    feat: implement cat item component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import { CatItem } from './components/CatItem/CatItem';
+
 function App() {
-  return <div className="container">Test</div>;
+  return (
+    <div className="container">
+      <CatItem id="1" url="https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg" />
+    </div>
+  );
 }
 
 export default App;
